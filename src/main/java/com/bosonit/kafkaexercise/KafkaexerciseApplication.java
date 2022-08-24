3b4b74547e195752481e16dee16f79db35b4ffce
Emar Martinez <emar.martinez@bosonit.com>
2022-08-24 13:29:49 +0200

    consumer+producer plain text messages

---
+++ src/main/java/com/bosonit/kafkaexercise/KafkaexerciseApplication.java
@@ -0,0 +1,13 @@
+package com.bosonit.kafkaexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaexerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaexerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bosonit/kafkaexercise/KafkaexerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bosonit.kafkaexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaexerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
